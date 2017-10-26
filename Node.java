0d014facf57c5910c42585d28013c3937929c7dc
a.sa'fin <23126506+safinayah@users.noreply.github.com>
2017-10-26 22:44:16 +0300

    Update Node.java

--- Node.java
+++ Node.java
@@ -5,34 +5,87 @@
  */
 package polynomialsum;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  *
  * @author ayah
  */
-public class Node {
-    
+public class Node {// a class to define exponent and coefficient to fill each term's data in nodes 
+
+    private int expo;//defines exponent 
+    private int coe;//defines coefficient
     Node next;//ref to next node
-
-    private int co; //cooficient
-
-    private int expo; //exponent
+    Node head;//ref to first node
     
 
-    Node(){ }//no args constructor 
-
-    Node(int co) {//a cosntructor 
-        this.co = co;
-        
+    Node() {//no args constuctor
     }
 
+    Node(int expo, int coe) {//a constructor which takes two int epxo and coefficient
 
-    Node(int co, int expo) {//a cosntructor takes coefficient and exponent 
-        this.co = co;
-        this.next=next;
         this.expo = expo;
+        this.coe = coe;
 
     }
 
+    public int getExpo() {//expo's getter
+        return expo;
+    }
 
+    public void setExpo(int expo) {//expo's setter
+        this.expo = expo;
+    }
 
-} // end class Node
+    public int getCoe() {//coe's getter
+        return coe;
+    }
+
+    public void setCoe(int coe) {//coe's setter
+        this.coe = coe;
+    }
+
+    public int numOfLines() {//calculates num of file's line
+        int num = 0;//initialize num
+
+        File x = new File("polynomial.txt"); //This is used to input the movies text
+
+        if (x.exists()) {//checks of the file exist
+
+            Scanner input = null;//initilalize input scanner 
+            try {
+                input = new Scanner(x); // Scanning
+
+            } catch (FileNotFoundException ex) {
+                System.out.println("noooooo data!!!!!!!!!!!");
+            }
+
+            while (input.hasNext()) {//checks if the file has nextLine
+                String line = input.nextLine(); // This is used to get to the next line 
+                if (line != null) {//checks if the file has data
+                    num++;//increments number of lines
+                }//end of if statement
+
+            }//end of while loop
+        }//end of class
+        return num;//returns number of file's lines
+    }//end of method
+
+    public void fillData(int expo, int coe) {
+
+        for (int i = 0; i < 2; i++) {
+            head.next.expo = expo;
+            head.next.coe = coe;
+        }
+     System.out.println("Exponent is :"+expo+"\ncoefficient is :"+coe);
+     
+    }
+    
+    public void displayLink(){
+        System.out.println("expo is : "+ expo+" coe is : "+ coe);
+    
+    }
+
+}//end of class
